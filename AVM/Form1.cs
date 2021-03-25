0c0a4c982ed0198c1a5a8fc1acc5d84fb4f357c8
S1LenTFeaR <killerus34@gmail.com>
2021-03-25 23:17:45 +0300

    4th AVM+

--- AVM/Form1.cs
+++ AVM/Form1.cs
@@ -22,45 +22,6 @@ namespace AVM
           
         }
 
-        /*private void Button1_Click_1(object sender, EventArgs e)
-        {
-            listView1.Items.Clear(); // Очищаем список
-                                     // Формируем WMI-запрос для получения информации из класса
-                                     // написанного в textBox1.Text
-            ManagementObjectSearcher searcher =
-            new ManagementObjectSearcher("SELECT * FROM " + textBox1.Text);
-            try // try-catch здесь используется для того чтобы
-            { // можно было отловить внезапно возникшую ошибку
-              // Получаем результаты запроса.
-                foreach (ManagementObject queryRes in searcher.Get())
-                {
-                    // Цикл по свойствам результата запроса
-                    foreach (PropertyData property in queryRes.Properties)
-                    {
-                        // Создаем строчку для отображения в listView1
-                        string[] row = new string[2];
-                        row[0] = property.Name; // В ячейку 0 записываем имя свойства
-                                                // Во вторую ячейку записываем значение свойства
-                                                // если значение отсутствует (равно null), то записываем null
-                        row[1] =
-                        (property.Value != null) ? property.Value.ToString() : "null";
-                        // Добавляем в listView1 строку с именем и значением свойства
-                        listView1.Items.Add(new ListViewItem(row));
-                    }
-                    // Для разделения результатов между собой после каждого
-                    // результата добавляем пустую строку (разделитель)
-                    listView1.Items.Add(new ListViewItem(new string[] { "", "" }));
-                }
-            }
-            catch
-            {
-                // Если вдруг в процесс получения результата запроса произошла // ошибка - выводим сообщение об ошибке
-                // (так же эта ошибка выведена и при других проблемах с WMI).
-                MessageBox.Show("Имя класса WMI введёно некорректно!");
-                return;
-            }
-        }*/
-
         private void Button1_Click_1(object sender, EventArgs e)
         {
             // Создаём объект ManagementObjectSearcher и передаём ему WMI-запрос,
@@ -104,7 +65,6 @@ namespace AVM
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "Введите сюда WMI-класс";
             listView1.Columns[0].Text = "Имя";
             listView1.Columns[1].Text = "Значение";
             listView1.Items.Clear(); // Очищаем список
@@ -142,6 +102,7 @@ namespace AVM
                 MessageBox.Show("Имя класса WMI введёно некорректно!");
                 return;
             }
+            textBox1.Text = "Введите сюда WMI-класс";
         }
 
         private void Button3_Click(object sender, EventArgs e)
